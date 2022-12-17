528eb804501b67c2a8babfc93bcda51895712664
ianfromdover <ian09hong@gmail.com>
2022-12-17 11:59:05 +0800

    Add qk65 and dz65rgb layouts

---
+++ keyboards/dztech/dz65rgb/keymaps/izumi/readme.md
@@ -0,0 +1,1 @@
+izumi's layout for tofu65
---
+++ keyboards/dztech/dz65rgb/keymaps/izumi/rules.mk
@@ -0,0 +1,9 @@
+DYNAMIC_MACRO_ENABLE = no
+LEADER_ENABLE = no
+
+# Firmware size Optimizations
+LTO_ENABLE = yes
+NKRO_ENABLE = yes
+TAP_DANCE_ENABLE   = no
+UNICODEMAP_ENABLE  = no
+SPLIT_KEYBOARD    = no
