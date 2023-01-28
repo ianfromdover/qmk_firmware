7dfd130c70db25e042015b88038447ba97b7cb9b
ianfromdover <ian09hong@gmail.com>
2023-01-28 11:23:08 +0800

    feat(keymap): x->w->b

--- keyboards/dztech/dz65rgb/keymaps/izumi/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/izumi/keymap.c
@@ -48,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ● ---------- Izumi Layer ---------- ●
     [0] = LAYOUT_65_ansi(
             QK_GESC,          KC_1,    KC_2,    KC_3,   KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    DF(1), 
-            LT(2,KC_TAB),     KC_W,    KC_L,    KC_Y,   KC_P,     KC_K,    KC_Z,    KC_F,    KC_O,    KC_U,    KC_QUOT, KC_LBRC, KC_RBRC, A(KC_BSPC), KC_DEL,
+            LT(2,KC_TAB),     KC_X,    KC_L,    KC_Y,   KC_P,     KC_K,    KC_Z,    KC_F,    KC_O,    KC_U,    KC_QUOT, KC_LBRC, KC_RBRC, A(KC_BSPC), KC_DEL,
             LT(3,KC_CAPS),    KC_C,    KC_R,    KC_S,   KC_T,     KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_A,    KC_SCLN, LT(4,KC_ENT),     G(KC_TAB), 
-            OSM(MOD_LSFT),    KC_J,    KC_V,    KC_D,   KC_B,     KC_Q,    KC_X,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, OSM(MOD_RSFT),    KC_UP,   G(KC_GRV),
+            OSM(MOD_LSFT),    KC_J,    KC_V,    KC_D,   KC_W,     KC_Q,    KC_B,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, OSM(MOD_RSFT),    KC_UP,   G(KC_GRV),
             LM(1,MOD_LCTL),   LM(1,MOD_LALT), LM(1,MOD_LGUI),     KC_SPC,           LT(7,KC_BSLS),    LT(5,KC_GRV),     TG(6),   KC_LEFT, KC_DOWN, KC_RGHT
             ),
 
