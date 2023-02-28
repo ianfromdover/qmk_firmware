0c49713c537d9d3b770ebfadcec5bc5833f85503
ianfromdover <ian09hong@gmail.com>
2023-03-01 00:14:52 +0800

    feat(iz68): add mouse layer toggle to nav

--- keyboards/dztech/dz65rgb/keymaps/izumi/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/izumi/keymap.c
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ● ---------- Navigation Layer ---------- ●
     [4] = LAYOUT_65_ansi(
             _______, _______, _______, _______,     _______,    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
-            _______, KC_HOME, KC_END, G(KC_LEFT),  G(KC_RGHT),  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+            _______, KC_HOME, KC_END, G(KC_LEFT),  G(KC_RGHT),  _______, _______, TG(6),   _______, _______, KC_BSPC, _______, _______, _______, _______, 
             _______, KC_LEFT, KC_DOWN, KC_UP,       KC_RGHT,    _______, _______, KC_RSFT, KC_RGUI, KC_RALT, KC_RCTL, _______,          _______, _______, 
             _______,          G(KC_Z), G(KC_X),     G(KC_C),    G(KC_V), _______, _______, KC_WH_D, KC_WH_U, _______, _______, _______, _______, _______, 
             _______, _______, _______,                          XXXXXXX,                            _______, _______, _______, _______, _______, _______
@@ -102,14 +102,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  XXXXXXX, 
             EE_CLR,  RGB_VAD, KC_BRID, KC_BRIU, RGB_VAI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG, 
             KC_MUTE, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, RGB_MOD, 
-            C(KC_SPC),        XXXXXXX, XXXXXXX, RGB_SPD, RGB_SPI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_SAI, RGB_RMOD,
+            C(S(KC_SCLN)),        XXXXXXX, XXXXXXX, RGB_SPD, RGB_SPI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_SAI, RGB_RMOD,
             CG_NORM, CG_SWAP, XXXXXXX,                   KC_MPLY,                            XXXXXXX, XXXXXXX, XXXXXXX, RGB_HUD, RGB_SAD, RGB_HUI
             ),
 
     // ● ---------- Mouse Layer ---------- ●
     [6] = LAYOUT_65_ansi(
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
-            _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, KC_WH_D, KC_MS_U, KC_WH_U, _______, _______, _______, _______, KC_ACL2, 
+            _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(6),   KC_WH_D, KC_MS_U, KC_WH_U, _______, _______, _______, _______, KC_ACL2, 
             _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_WH_L, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R, _______,          _______, KC_ACL1, 
             _______,          KC_Z,    KC_X,    KC_C,    KC_V,    _______, KC_BTN1, KC_BTN2, _______, _______, _______, _______, _______, KC_ACL0, 
             _______, _______, _______,                   _______,                            _______, _______, _______, _______, _______, _______
